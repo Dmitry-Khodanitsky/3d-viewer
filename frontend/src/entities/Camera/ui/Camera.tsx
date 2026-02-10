e5e698c657ea5551db5f4701eecf6a83dc3b7822
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-10 15:10:37 +0300

    feat: добавить сущность камеры

---
+++ frontend/src/entities/Camera/ui/Camera.tsx
@@ -0,0 +1,17 @@
+import {
+  PerspectiveCamera,
+  OrthographicCamera,
+  CameraControls,
+} from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+import { useRef, useLayoutEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectCameraView } from '@/entities/Camera/model/viewportSlice'
+import * as THREE from 'three'
+
+export const Camera = () => {
+  const controlsRef = useRef(null)
+  const { camera } = useThree()
+  const isOrtho = useSelector(selectCameraView)
+  return <PerspectiveCamera makeDefault fov={100} position={[2, 2, 2]} />
+}
