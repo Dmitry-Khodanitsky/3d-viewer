a096fcbec92ca55f24861c870cddff8c407ff366
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-10 15:07:37 +0300

    feat: добавить провайдер глобального состояния

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './app/model/store.ts' 
+import App from './app/ui/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
