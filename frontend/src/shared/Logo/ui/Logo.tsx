d8f0112e17dcc543308c37babaca5db11b1a1eec
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-10 15:39:29 +0300

    refactor: изменить размер лого

--- frontend/src/shared/Logo/ui/Logo.tsx
+++ frontend/src/shared/Logo/ui/Logo.tsx
@@ -1,3 +1,3 @@
 export const Logo = () => {
-  return <img src="/public/logo.svg" alt="3d viewer logo" width={55} height={55}  />
+  return <img src="/public/logo.svg" alt="3d viewer logo" width={45} height={45}  />
 }
