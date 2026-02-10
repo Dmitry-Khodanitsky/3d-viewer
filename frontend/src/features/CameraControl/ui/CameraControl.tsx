8009fc2e79c29d42e62c03a9c482aa49ffe05e51
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-10 15:11:17 +0300

    feat: добавить фичу управления свойствами камеры

---
+++ frontend/src/features/CameraControl/ui/CameraControl.tsx
@@ -0,0 +1,16 @@
+import { ActionButton } from '@/shared'
+import { IconCube } from '@tabler/icons-react'
+import { useDispatch } from 'react-redux'
+import { toggleCameraView } from '@/entities/Camera/model/viewportSlice'
+
+export const CameraControl = () => {
+  const dispatch = useDispatch()
+  const handleToggle = () => {
+    dispatch(toggleCameraView()) // Экшен вызывается как функция ()
+  }
+  return (
+    <ActionButton handleClick={handleToggle}>
+      <IconCube stroke={1.5} style={{ width: '70%', height: '70%' }} />
+    </ActionButton>
+  )
+}
