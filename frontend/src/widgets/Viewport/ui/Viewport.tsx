8edd79e606fa982538f3d9231c68902f753171a5
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-10 15:08:13 +0300

    feat: добавить виджет вьюпорта

---
+++ frontend/src/widgets/Viewport/ui/Viewport.tsx
@@ -0,0 +1,32 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Bounds, Environment } from '@react-three/drei'
+import { SceneLight, Mesh } from '@/entities/'
+import { Suspense } from 'react'
+import { useSelector } from 'react-redux'
+import { selectCameraView } from '@/entities/Camera/model/viewportSlice'
+import { Camera } from '@/entities/Camera/ui/Camera'
+
+export const Viewport = () => {
+  const isOrthographic = useSelector(selectCameraView)
+
+  return (
+    <div style={{ width: '75vw', height: '75vh' }}>
+      <Canvas style={{ background: 'gray' }}>
+        {/* Добавляет свет и отражения типа "город" */}
+        <Camera />
+        <Suspense fallback={null}>
+          <Environment files="/gard.hdr" />
+        </Suspense>
+
+        <OrbitControls minDistance={1} maxDistance={10} />
+        <SceneLight />
+
+        <Bounds fit observe margin={2}>
+          <Mesh />
+        </Bounds>
+
+        <axesHelper args={[10]} />
+      </Canvas>
+    </div>
+  )
+}
