87ee3de2582444f940e70c0781b2dc65537d21c3
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-16 13:45:12 +0300

    refactor: toolbar используется в Viewport

--- frontend/src/pages/Workspace/ui/Workspace.tsx
+++ frontend/src/pages/Workspace/ui/Workspace.tsx
@@ -1,15 +1,15 @@
 import { Toolbar, Viewport } from '@/widgets'
-import { Container } from '@mantine/core'
-import { ModelUploader } from '@/features'
+import { Container, Flex } from '@mantine/core'
 import { selectModelUrl } from '@/entities/Mesh/model/meshSlice'
 import { useSelector } from 'react-redux'
+import { UploadZone } from '@/widgets/UploadZone'
+import { ModelUploader } from '@/features'
 
 export const Workspace = () => {
   const modelUrl = useSelector(selectModelUrl)
   return (
-    <Container>
-      <Toolbar />
+    <Flex justify="center" align="center" w="100%" h="100%">
       {modelUrl ? <Viewport /> : <ModelUploader />}
-    </Container>
+    </Flex>
   )
 }
