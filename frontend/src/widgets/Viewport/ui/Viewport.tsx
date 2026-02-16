fa64fc5d5f5dde750bd338c04a542ddf27daba81
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-16 13:27:59 +0300

    feat: добавить оверлей загрузки при загрузке 3д модели

--- frontend/src/widgets/Viewport/ui/Viewport.tsx
+++ frontend/src/widgets/Viewport/ui/Viewport.tsx
@@ -1,33 +1,62 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Bounds, Environment } from '@react-three/drei'
-import { SceneLight, Mesh } from '@/entities/'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import {
+  OrbitControls,
+  Bounds,
+  Environment,
+  useProgress,
+  Wireframe,
+} from '@react-three/drei'
+import { SceneLight, Mesh, Camera, Toolbar } from '@/entities/'
 import { Suspense } from 'react'
 import { useSelector } from 'react-redux'
-import { selectCameraView } from '@/entities/Camera/model/viewportSlice'
-import { Camera } from '@/entities/Camera/ui/Camera'
-import { Container } from '@mantine/core'
+import { selectCameraView } from '@/entities/Camera/model/cameraSlice'
+import { LoadingOverlay, Paper, Stack } from '@mantine/core'
 
 export const Viewport = () => {
   const isOrthographic = useSelector(selectCameraView)
+  const { active } = useProgress()
 
   return (
-    <Container h='75vh'>
-      <Canvas style={{ background: 'gray' }}>
-        {/* Добавляет свет и отражения типа "город" */}
-        <Camera />
-        <Suspense fallback={null}>
-          <Environment files="/gard.hdr" />
+    <Paper w="100%" h="90vh">
+      <Stack h="100%" pb="xs" gap="0">
+        <Suspense
+          fallback={
+            <LoadingOverlay
+              visible={active}
+              zIndex={1000}
+              overlayProps={{ radius: 'sm', blur: 2 }}
+            />
+          }
+        >
+          <Canvas>
+            {/* Добавляет свет и отражения типа "город" */}
+            <Camera />
+
+            <Suspense fallback={null}>
+              <Environment files="/gard.hdr" />
+            </Suspense>
+            <OrbitControls minDistance={1} maxDistance={10} />
+            <SceneLight />
+
+            <Bounds fit observe margin={2}>
+              <Mesh />
+            </Bounds>
+
+            <axesHelper args={[10]} />
+            <EffectComposer>
+              <Bloom
+                luminanceThreshold={2.5} // Порог выше 1.0 отсекает обычные белые блики
+                luminanceSmoothing={0.1} // Делаем границу свечения четче (меньше "мыла")
+                intensity={0.5} // Уменьшаем общую силу ореола
+                radius={0.4} // Компактный радиус (ореол не на пол-экрана)
+                mipmapBlur
+              />
+            </EffectComposer>
+          </Canvas>
+          <Toolbar />
         </Suspense>
-
-        <OrbitControls minDistance={1} maxDistance={10} />
-        <SceneLight />
-
-        <Bounds fit observe margin={2}>
-          <Mesh />
-        </Bounds>
-
-        <axesHelper args={[10]} />
-      </Canvas>
-    </Container>
+      </Stack>
+    </Paper>
   )
 }
