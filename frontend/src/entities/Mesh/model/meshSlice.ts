d794d14d521e924b9ccd8fc060515a8eb1476be6
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-02-16 13:48:02 +0300

    refactor: добавить редьюсер для отображения/скрытия сетки

--- frontend/src/entities/Mesh/model/meshSlice.ts
+++ frontend/src/entities/Mesh/model/meshSlice.ts
@@ -4,16 +4,21 @@ import type { PayloadAction } from '@reduxjs/toolkit'
 
 interface MeshState {
   modelUrl: string | null
+  isWireframe: boolean
 }
 
 const initialState: MeshState = {
   modelUrl: null,
+  isWireframe: false, // Флаг для сетки
 }
 
 const meshSlice = createSlice({
   name: 'model',
   initialState,
   reducers: {
+    toggleWireframe: (state) => {
+      state.isWireframe = !state.isWireframe
+    },
     setModalUrl: (state, { payload }: PayloadAction<string>) => {
       state.modelUrl = payload
     },
@@ -24,5 +29,6 @@ const meshSlice = createSlice({
 })
 
 export default meshSlice.reducer
-export const { setModalUrl, removeModalUrl } = meshSlice.actions
+export const { setModalUrl, toggleWireframe } = meshSlice.actions
 export const selectModelUrl = (state: RootState) => state.model.modelUrl
+export const selectIsWireframe = (state: RootState) => state.model.isWireframe
